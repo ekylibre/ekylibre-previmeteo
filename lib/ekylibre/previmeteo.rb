5b6c66576a4c36c8bc3c186dbbad621ad769060e
Rémi Piotaix <remi@piotaix.fr>
2020-12-17 18:15:57 +0100

    Update to plugin_system 0.4

--- lib/ekylibre/previmeteo.rb
+++ lib/ekylibre/previmeteo.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'ekylibre-plugin_system'
+
 module Ekylibre
   module Previmeteo
     autoload :GenericController, "#{__dir__}/previmeteo/generic_controller"
